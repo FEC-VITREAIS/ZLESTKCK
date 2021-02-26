4afc4a1eb303e4345f7f9e7f752915ee1d01d473
Christina Kirk <cmariekirk@gmail.com>
2021-02-26 14:03:59 -0500

    Built out more structure to related-card divs

--- client/src/Related/Related-List-Card.jsx
+++ client/src/Related/Related-List-Card.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 var RelatedListCard = () => {
   return (
-    <div className='related-list-card'></div>
+    <div className='related-list-card'>
+      <img className='related-list-card-img' src='https://img.pokemondb.net/artwork/charmander.jpg'/>
+      <div className='related-card-div'>Category</div>
+      <div className='related-card-div'>Expanded Product Name with Extra Text</div>
+      <div className='related-card-div'>Price</div>
+      <div className='related-card-div'>Star Rating</div>
+    </div>
   )
 }
 
