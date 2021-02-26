83e6543eecd6ea97ce918a528b5c34bf564a0028
Christina Kirk <cmariekirk@gmail.com>
2021-02-26 13:56:11 -0500

    Added placeholder related list cards to related list component

---
+++ client/src/Related/Related-List-Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+var RelatedListCard = () => {
+  return (
+    <div className='related-list-card'></div>
+  )
+}
+
+export default RelatedListCard;
--- client/src/Related/Related-List.jsx
+++ client/src/Related/Related-List.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import RelatedListCard from './Related-List-Card.jsx';
 
 
 var RelatedList = ({className}) => {
   return (
-    <div className={className}> Related List Component </div>
+    <div className={className}> 
+    <RelatedListCard/>
+    <RelatedListCard/>
+    <RelatedListCard/>
+    </div>
   )
 }
 
