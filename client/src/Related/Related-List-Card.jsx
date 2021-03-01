d080b7b79fb110c4081a69f7650be8f0c3380e13
KirkNotCaptain <cmariekirk@gmail.com>
2021-03-01 13:10:09 -0500

    Adjusted card styles for closer approx to BDR specs

--- client/src/Related/Related-List-Card.jsx
+++ client/src/Related/Related-List-Card.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 var RelatedListCard = ({product}) => {
   return (
     <div className='related-list-card'>
-      <img className='related-list-card-img' src='https://img.pokemondb.net/artwork/charmander.jpg'/>
-      <div className='related-card-div'>Category {product.category}</div>
-      <div className='related-card-div'>Expanded Product Name with Extra Text</div>
-      <div className='related-card-div'>Price</div>
+      <img className='related-list-card-img'/>
+      <div className='related-card-category'>{product.category}</div>
+      <div className='related-card-name'>{product.name}</div>
+      <div className='related-card-price'>{`$${product.default_price}`}</div>
       <div className='related-card-div'>Star Rating</div>
     </div>
   )
