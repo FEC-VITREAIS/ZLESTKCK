955edbf9eaa025568193fd011ebab6916c130ab4
Eddy <eddy@Eddys-MBP.lan>
2021-03-02 18:45:17 -0500

    commiting bundle and Product container

--- client/src/Product/ProductContainer.jsx
+++ client/src/Product/ProductContainer.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 let ProductContainer = function(props) {
 
-
+  const [CurrentProducts, setCurrentProducts] = useState([]);
 
   return (
+
     <div>Product Detail Container</div>
   )
 }
