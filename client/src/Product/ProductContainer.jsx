10947f00a27ba62b97f83c2a86175f8206ce4c56
Eddy <eddy@Eddys-MBP.lan>
2021-03-02 19:36:12 -0500

    adding skeleton to DisplayProductPreview component and mapping out what default photo to choose from

--- client/src/Product/ProductContainer.jsx
+++ client/src/Product/ProductContainer.jsx
@@ -2,7 +2,12 @@ import React, { useState, useEffect } from 'react';
 
 let ProductContainer = function(props) {
 
-  const [CurrentProducts, setCurrentProducts] = useState([]);
+  const [CurrentProducts, setCurrentProducts] = useState([]); /* all the list of products for display */  
+  const [CurrentProductView, setCurrentProductView] = useState({}); /* Current product when clicked on in CurrentProducts */ 
+  
+  const HandleProductChange = (e) => {
+
+  }
 
   return (
 
--- client/src/Product/components/DisplayProductPreview/DisplayProductPreview.jsx
+++ client/src/Product/components/DisplayProductPreview/DisplayProductPreview.jsx
@@ -1,16 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 /* going to take in product pictures to display them and on click
 will display the current product being viewed and will display the size of that product
 */
 
-let DisplayProductPreview = (props) => {
-
+let DisplayProductPreview = (products) => {
   return (
+    <>
+      <div className="DisplayProductPreviewContainer">
 
-    <div>Product Preview</div>
-  )
-}
+        {products.map( product => {
+          
+          const StylesArr = product.results; /* Objects that have styles and photots theres a go to photo somewhere in here */
+        })}
 
+      </div>
+    </>
+  );
+};
 
 export default DisplayProductPreview;
