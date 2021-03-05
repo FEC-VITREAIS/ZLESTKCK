37c3b2ac9130182c98b8599ac26440c3ca3262d6
Tim Kang <tikang.cs@gmail.com>
2021-03-05 12:31:53 -0500

    3.1 to 3.4 #4 - Implemented the accordion to expand and collapse the answers presented for each question

--- client/src/QA/QA-AnswerEntries.jsx
+++ client/src/QA/QA-AnswerEntries.jsx
@@ -4,22 +4,24 @@ let AnswerEntries = ({responder, body, date, helpfulCount, images, aCount}) => {
   return (
     <>
     <div className="QAaentries">
-      <span className="QAaentries_answer">A:</span>
-      <span className="QAaentries_meta">From: {responder}</span>
-      <span className="QAaentries_meta">{date}</span>
-      <span className="QAaentries_meta">Mark as Helpful ({helpfulCount})</span>
-      <span className="QAaentries_meta">Report</span>
+      {/* <span className="QAaentries_answer">A:</span> */}
+      <div className="QAaentries_metaContainer">
+        <span className="QAaentries_meta">From: {responder}</span>
+        <span className="QAaentries_meta">{date}</span>
+        <span className="QAaentries_meta">Mark as Helpful ({helpfulCount})</span>
+        <span className="QAaentries_meta">Report</span>
+      </div>
+      <div className="QAaentries_answerBody">A: {body}</div>
+      <div className="QAaentries_images">
+        {images.map((image) => {
+          return (
+            <img className="QAaentries_thumbnails" src={image} key={image.length}></img>
+          )
+        })}
+      </div>
     </div>
 
-    <div className="QAaentries_answerBody">{body}</div>
 
-    <div className="QAaentries_images">
-      {images.map((image) => {
-        return (
-          <img className="QAaentries_thumbnails" src={image} key={image.length}></img>
-        )
-      })}
-    </div>
 
     {/* <div className="QAaentries_loadMore">
       <span className="QAaentries_displayCount">Answers 1-2 of {aCount}</span>
--- client/src/QA/QA-QuestionEntries.jsx
+++ client/src/QA/QA-QuestionEntries.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import ProductContext from '../context.jsx';
 import QAstyles from './styles/QAstyles.css'
 import AnswerEntries from './QA-AnswerEntries.jsx'
@@ -6,32 +6,86 @@ import AModal from './QA-AnswerModal.jsx'
 
 let QuestionEntries = function({body, asker, date, helpfulCount, reported, answers, qCount}) {
   const QAqentriesContext = React.useContext(ProductContext);
-  const [arrayOfAnswers, setArrayOfAnswers] = useState(answers)
+  let aCount = answers.length;
+  const orderedAnswers =  answers.sort((a, b) => {
+    return (a.helpfulness > b.helpfulness) ? -1: 1
+  })
+
+  const [arrayOfAnswers, setArrayOfAnswers] = useState(orderedAnswers.slice(0,2))
+  const [displayedIndex, setDisplayedIndex] = useState(0)
+  const [isFullyLoaded, setIsFullyLoaded] = useState(false)
+  const [isCollapsed, setIsCollapsed] = useState(false)
+
+  const displayedAnswers = (index) => {
+      setArrayOfAnswers(orderedAnswers.slice(0, index))
+      setDisplayedIndex(index)
+  }
+
+  const loadClickHandler = (e) => {
+    if (displayedIndex === aCount) {
+      displayedAnswers(aCount);
+    } else if (displayedIndex >= aCount-1) {
+      displayedAnswers(aCount)
+      setIsFullyLoaded(true);
+    } else {
+      displayedAnswers(displayedIndex+2)
+    }
+  }
+
+  const collapseClickHandler = (e) => {
+    displayedAnswers(2)
+    setIsFullyLoaded(false)
+    setIsCollapsed(true)
+  }
+
+  useEffect(() => {
+    displayedAnswers(2)
+    }, [isCollapsed])
 
   return (
     <>
     <div className="QAqentries">
-      <span className="QAqentries_question">Q:</span>
-      <span className="QAqentries_meta">From: {asker}</span>
-      <span className="QAqentries_meta">{date}</span>
-      <span className="QAqentries_meta">Mark as Helpful ({helpfulCount})</span>
-      <span className="QAqentries_meta">Report</span>
+      <div className="QAqentries_metaContainer">
+        {/* <span className="QAqentries_question">Q:</span> */}
+        <span className="QAqentries_meta">From: {asker}</span>
+        <span className="QAqentries_meta">{date}</span>
+        <span className="QAqentries_meta">Mark as Helpful ({helpfulCount})</span>
+        <span className="QAqentries_meta">Report</span>
+      </div>
+      <div className="QAqentries_questionBody">Q: {body}
+      <button className="accordion">X</button>
+      <div className="QAqentries_answers">
+        1-{displayedIndex} of {aCount} Answers
+        <button className="QAqentries_addButton" type="button">Add an answer</button>
+      </div>
+        {arrayOfAnswers.map((answer) => {
+          return (<AnswerEntries
+          responder={answer.answerer_name}
+          body={answer.body}
+          date={answer.date}
+          helpfulCount={answer.helpfulness}
+          images={answer.photos}
+          aCount={aCount}
+          key={answer.id} />)
+        })
+        }
+        </div>
     </div>
 
-    <div className="QAqentries_questionBody">{body}
-      <button className="QAqentries_addButton" type="button">Add an answer</button>
-      {arrayOfAnswers.map((answer) => {
-        return (<AnswerEntries
-        responder={answer.answerer_name}
-        body={answer.body}
-        date={answer.date}
-        helpfulCount={answer.helpfulness}
-        images={answer.photos}
-        aCount={arrayOfAnswers.length}
-        key={answer.id} />)
-      })
+    <div>
+      {isFullyLoaded ?
+      <div className="QAqentries_collapse">
+        <span className="QAqentries_displayCount">Answers 1-{aCount}</span>
+        <button className="QAqentries_collapseButton" type="button" onClick={collapseClickHandler}>Collapse answers</button>
+      </div>
+      :
+      <div className="QAqentries_loadMore">
+        <span className="QAqentries_displayCount">Answers 1-{displayedIndex} of {aCount}</span>
+        <button className="QAqentries_loadButton" type="button" value={displayedIndex} onClick={loadClickHandler}>Load more answers</button>
+      </div>
       }
     </div>
+
     <AModal productName={QAqentriesContext.productName} body={body}/>
     </>
   )
