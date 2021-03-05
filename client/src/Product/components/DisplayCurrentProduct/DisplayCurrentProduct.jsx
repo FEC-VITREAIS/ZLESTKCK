a225b5f8e6d1a4257d8e32f90c003f87fe3c6288
Eddy <eddy@Eddys-MBP.lan>
2021-03-04 23:45:35 -0500

    component is read only https://stackoverflow.com/questions/33900933/cannot-assign-to-read-only-property-props-of-object-in-react-native
    err
    
    it cannot be changed due to reacts nature only main component can have default props cannot change children components props

--- client/src/Product/components/DisplayCurrentProduct/DisplayCurrentProduct.jsx
+++ client/src/Product/components/DisplayCurrentProduct/DisplayCurrentProduct.jsx
@@ -1,24 +1,23 @@
 import React, { useState, useEffect } from "react";
 
-import { defaultProps } from 'recompose';
-
-const DisplayCurrentProduct = ({ currentProduct } = {photos: [{thumbnail_url: ''}]}) => {
+const DisplayCurrentProduct = ({ currentProduct }) => {
   console.log(currentProduct, "this is the current product");
 
   let { thumbnail_url } = currentProduct.photos[0];
 
-  if (thumbnail_url === '') {
-    return <div> </div>
+  if (thumbnail_url === "") {
+    return <div> </div>;
   }
 
   // const [CurrentStyle, setStyle] = useState()
 
   return (
     <div className="DisplayCurrentProductContainer">
-      <div className="photo-list">  </div>
+      <div className="photo-list"> </div>
       <div className="mainSlider"></div>
     </div>
   );
 };
+
 
 export default DisplayCurrentProduct;
