a5edccf163d011e7d6e7cb0685dc88eec98a981a
KirkNotCaptain <cmariekirk@gmail.com>
2021-03-05 19:02:40 -0500

    Added css for pop up modal window

--- client/src/Related/Related-List-Card.jsx
+++ client/src/Related/Related-List-Card.jsx
@@ -8,19 +8,39 @@ var RelatedListCard = ({product, photo, price}) => {
   // console.log('related list card product: ', product);
 
 
-  const handleClick = (e) => {
+  const handleProductChange = (e) => {
     // console.log(product.id);
     context.updateCurrentProduct(product.id)
+  };
+
+  const handleProductCompare = (e) => {
+    var modal = document.getElementById('comparison-modal');
+    modal.style.display = "block";
+
   }
 
+  const handleModalClose = (e) => {
+    var modal = document.getElementById('comparison-modal');
+    modal.style.display = "none";
+  }
+
+
+
   return (
-    <div className='related-list-card' onClick={handleClick}>
-      <span class="fa fa-star checked"></span>
+    <div className='related-list-card'>
+      <span class="fa fa-star checked" onClick={handleProductCompare}></span>
       <img className='related-list-card-img' src={photo}/>
       <div className='related-card-category'>{product.category}</div>
-      <div className='related-card-name'>{product.name}</div>
+      <div className='related-card-name' onClick={handleProductChange}>{product.name}</div>
       <div className='related-card-price'>{`$${price}`}</div>
       <div className='related-card-div'>Star Rating</div>
+      <div id='comparison-modal' class='comparison-modal'>
+        <div class="modal-content">
+        <span id='close-modal' class='close' onClick={handleModalClose}>&times;</span>
+        <p>Some text in the Modal..</p>
+      </div>
+
+</div>
     </div>
   )
 }
