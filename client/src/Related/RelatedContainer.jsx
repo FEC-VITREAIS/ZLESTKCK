ffd7258383543b8d2855deafa39b833ceb2de627
KirkNotCaptain <cmariekirk@gmail.com>
2021-03-13 12:31:18 -0500

    Improved comments for further clarity

--- client/src/Related/RelatedContainer.jsx
+++ client/src/Related/RelatedContainer.jsx
@@ -11,34 +11,15 @@ import fetchRelatedProducts from '../FetchData/fetchRelatedProducts.js';
 let RelatedContainer = function(props) {
   const context = useContext(ProductContext);
 
-  /**
-   * Local State for main container of related items
-   *
-   * displayModal
-   * @type {bool} represents whether the modal window for related items should appear
-   *
-   * sharedFeatures
-   * @type {object} contains an array-like structure where the key is a feature name and the value is another object containing the value of that feature for each product being compared
-   *
-   * modalProduct
-   * @type {object} an empty state object that is passed to the modal window to ensure that the specific related product clicked on will return back to the related container state (which currently has no knowledge of that product)
-   */
-
   const [relatedProducts, setRelatedProducts] = useState([]);
-  const [displayModal, setDisplayModal] = useState(false);
-  const [sharedFeatures, setSharedFeatures] = useState({});
-  const [modalProduct, setModalProduct] = useState({});
+  const [displayModal, setDisplayModal] = useState(false); //determines if modal window is active
+  const [sharedFeatures, setSharedFeatures] = useState({}); //comparison object between related product and current product
+  const [modalProduct, setModalProduct] = useState({}); //ensures that the specific related product clicked on will return back to the related container state (to pass to the modal window as it's product).
 
-  /**
-   * handleModalClose
-   *
-   * @param -none
-   * @return {undefined}
-   *
-   * updates displayModal in local state to false thus closing the modal window
-   * updates sharedFeatures in localState to an empty object ensuring that any new comparison will only contain the newest two items being compared
-   *
-   */
+  
+  //updates displayModal in local state to false thus closing the modal window
+  //updates sharedFeatures in localState to an empty object 
+  //ensuring that any new comparison will only contain the newest two items being compared
   const handleModalClose = (e) => {
     setDisplayModal(false);
     setSharedFeatures({});
@@ -65,46 +46,30 @@ let RelatedContainer = function(props) {
         modalProduct,
         setModalProduct
       }}>
-        <div className='related-container'>
-          <RelatedList />
-          <YourOutfitsList />
-        </div>
+        <>
+        <ReactModal
+          isOpen={displayModal}
+          onRequestClose={handleModalClose}
+          shouldCloseOnEsc={true}>
+            <RelatedModalWindow
+            handleModalClose={handleModalClose}
+            product={modalProduct}
+            sharedFeatures={sharedFeatures}
+            />
+        </ReactModal>
+
+          <div className='related-container'>
+            <RelatedList />
+            <YourOutfitsList />
+          </div>
+        </>
       </RelatedProductContext.Provider>
     )
   } else {
     return (
-      <></>
+      <div className='related-container'></div>
     )
   }
-
-//   if (context.relatedProducts && context.relatedProducts.length) {
-//     return (
-//       <>
-//       <ReactModal
-//         isOpen={displayModal}
-//         onRequestClose={handleModalClose}
-//         shouldCloseOnEsc={true}>
-//            <RelatedModalWindow
-//            handleModalClose={handleModalClose}
-//            product={modalProduct}
-//            sharedFeatures={sharedFeatures}
-//           />
-//       </ReactModal>
-
-        // <div className='related-container'>
-        //   <RelatedList
-        //   setSharedFeatures={setSharedFeatures}
-        //   setModalProduct={setModalProduct}
-        //   setDisplayModal={setDisplayModal}/>
-        //   <YourOutfitsList />
-        // </div>
-//       </>
-//     )
-//   } else {
-//     return (
-//       <div className='related-container'></div>
-//     )
-//   }
 }
 
 
